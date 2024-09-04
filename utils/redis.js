a7620988fe85ca74d9444dc275cbfcfd2ad90fb5
emmaculate-exceel <emmaculate2exceel@live.com>
2024-09-04 17:00:31 +0100

    update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,53 +1,35 @@
-const redis = require("redis");
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
-class RedisClient{
-    constructor () {
-	this.client = redis.createClien();
-	this.client.on('error', (err) => {
-	    console.error('Reddis client error:', err);
-	});
+class RedisClient {
+    constructor() {
+	
+        this.client = createClient();
+        this.client.on('error', (err) => {
+            console.error('Redis client error:', err);
+        });
+
+        this.getAsync = promisify(this.client.get).bind(this.client);
+        this.setAsync = promisify(this.client.set).bind(this.client);
+        this.delAsync = promisify(this.client.del).bind(this.client);
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return await this.getAsync(key);
+    }
+
+    async set(key, value, duration) {
+        await this.setAsync(key, value, 'EX', duration);
+    }
+
+    async del(key) {
+        await this.delAsync(key);
     }
 }
 
-isAlive() {
-    return this.client.connected;
-}
-
-async get(key) {
-    return new Promise((resolve, reject) => {
-	this.client.get(key, (err, value) => {
-	    if (err) {
-		reject(err);
-	    }else {
-		resolve(value);
-	    }
-	});
-    });
-}
-
-async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-	this.client.set(key, value, 'EX', duration, (err) => {
-	    if (err) {
-		reject(err);
-	    }else {
-		resolve();
-	    }
-	});
-    )};
-}
-
-async del(key) {
-    return new Promise((resolve, reject) => {
-	this.client.del(key, (err) => {
-	    if (err) {
-		reject(err);
-	    }else {
-		resolve();
-	    }
-	});
-    });
-}
-
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
